31c0b9eff38b502278d885ee7a6fd145cc7bff6f
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-12-26 23:31:51 +0100

    ✨ hide search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import agent from "../../agent";
 import logo from "../../imgs/logo.png";
 
 const Banner = ({ onFilter, search, onChangeValue }) => {
+  const [hidden, setHidden] = useState(true);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get </span>
-          <input
-            type="search"
-            name="search"
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            onChange={async (e) => {
-              const title = e.target.value.trim();
-              onChangeValue(title);
-              if (title.length > 2 || !title) {
-                const filteredItems = await agent.Items.byTitle(e.target.value);
-                onFilter(e.target.value, filteredItems);
-              }
-            }}
-          />
+          <span id="get-part">
+            A place to <span onClick={() => setHidden(!hidden)}>get</span>{" "}
+          </span>
+          {!hidden && (
+            <input
+              type="search"
+              name="search"
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              onChange={async (e) => {
+                const title = e.target.value.trim();
+                onChangeValue(title);
+                if (title.length > 2 || !title) {
+                  const filteredItems = await agent.Items.byTitle(
+                    e.target.value
+                  );
+                  onFilter(e.target.value, filteredItems);
+                }
+              }}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-constant-condition */
 import React from "react";
 import { connect } from "react-redux";
 import agent from "../../agent";
